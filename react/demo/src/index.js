c261d0970529a63699ce10f475c72a2985f776b4
admin <liyayeguang@MacBook-Pro.local>
2020-02-26 23:48:17 +0800

    1

--- react/demo/src/index.js
+++ react/demo/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<div>hahas</div>, document.getElementById('root'));
+ReactDOM.render(<>hahas</>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
