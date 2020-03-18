5f4f23b4f250dab4b874fe5c5baafb4160b9f091
sjf <1114828808@qq.com>
2020-03-18 13:05:06 +0800

    5555

--- js基础/关于比较/compare.js
+++ js基础/关于比较/compare.js
@@ -1,7 +1,7 @@
 // 对象进行转换的时候 都是先调用valueOf 当valueOf返回的还是对象时,接着调用toString方法(否则不调用)
 Array.prototype.toString = function () {
   console.log('调用了toString方法')
-  return 4
+  return 3
 }
 Array.prototype.valueOf = function () {
   console.log('调用了valueOf方法')
